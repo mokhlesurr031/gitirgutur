0742a00beaf8514382151ddbe77870bc3ab96cf5
Mokhlesur Mahin <mokhlesur.rahman@pathao.com>
2023-03-05 14:33:47 +0600

    0

--- main.go
+++ main.go
@@ -4,15 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("HELLO WORLD 0")
-	fmt.Println("HELLO WORLD 1")
-	fmt.Println("HELLO WORLD 2")
-	fmt.Println("HELLO WORLD 3")
-	fmt.Println("HELLO WORLD 4")
-	fmt.Println("HELLO WORLD 5")
 
-	fmt.Println("HELLO WORLD b 0")
-	fmt.Println("HELLO WORLD b 1")
-	fmt.Println("HELLO WORLD b 2")
-	fmt.Println("HELLO WORLD b 3")
-	fmt.Println("HELLO WORLD b 4")
 }
